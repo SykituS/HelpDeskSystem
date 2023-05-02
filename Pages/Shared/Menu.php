5c85ac135ba52684830a42b6a9d12ca54218be2f
Mateusz Jaruga <UL0253575@edu.uni.lodz.pl>
2023-05-02 22:20:39 +0200

    removed unused html

--- Pages/Shared/Menu.php
+++ Pages/Shared/Menu.php
@@ -1,18 +1,3 @@
-<!--<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-    <ul class="nav navbar-nav menus">
-			<li id="ticket"><a href="ticket.php" class="navbar-brand">Ticket</a></li>
-			<?php if(isset($_SESSION["admin"])) { ?>
-				<li id="department"><a href="department.php" >Department</a></li>
-				<li id="user"><a href="user.php" >Users</a></li>				
-			<?php } ?>					
-		</ul>
-		<ul class="nav navbar-nav navbar-right">				
-        <li><a href="../Account/Logout.php">Logout</a></li>
-		</ul>
-    </div>
-</nav>-->
-
 <!--NAV BAR-->
 <div class="navbar navbar-expand-md navbard-light fixed-top bg-light">
 	<div class="container-fluid justify-content-between">
@@ -80,6 +65,6 @@
 			</div>
 		</div>
 		<!--elements: right-->
-		</div>
+	</div>
 </div>
 <!--NAV BAR-->
